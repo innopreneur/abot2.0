fa21e647396ccc2b56670f52137d7e4f8261dd24
Manan Patel <manan77patel@gmail.com>
2019-11-02 17:03:32 +0100

    added dockerfile

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,40 @@
-const winston = require("winston");
-const logger = winston.createLogger({
+var appRoot = require("app-root-path");
+var winston = require("winston");
+
+// define the custom settings for each transport (file, console)
+var options = {
+  file: {
+    level: "info",
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 52428800, // 50MB
+    maxFiles: 10,
+    colorize: false
+  },
+  console: {
+    level: "debug",
+    handleExceptions: true,
+    json: false,
+    colorize: true
+  }
+};
+
+// instantiate a new Winston Logger with the settings defined above
+var logger = new winston.createLogger({
   transports: [
-    new winston.transports.Console({
-      name: "console",
-      colorize: true,
-      timestamp: function() {
-        return new Date(Date.now());
-      }
-    }),
-    new winston.transports.File({ filename: `${Date.now()}.log` })
-  ]
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
+  ],
+  exitOnError: false // do not exit on handled exceptions
 });
+
+// create a stream object with a 'write' function that will be used by `morgan`
+logger.stream = {
+  write: function(message, encoding) {
+    // use the 'info' log level so the output will be picked up by both transports (file and console)
+    logger.info(message);
+  }
+};
 
 export default logger;
